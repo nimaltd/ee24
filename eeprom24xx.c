8030066d429369625c4355b7abc6f7d64a35287b
Nima Askari ----- نیما عسکری <nimaltd@yahoo.com>
2018-06-03 12:41:57 +0430

    Add WP pin

--- eeprom24xx.c
+++ eeprom24xx.c
@@ -7,6 +7,9 @@
 //##########################################################################
 bool		EEPROM24XX_IsConnected(void)
 {
+	#if	(_EEPROM_USE_WP_PIN==1)
+	HAL_GPIO_WritePin(_EEPROM_WP_GPIO,_EEPROM_WP_PIN,GPIO_PIN_SET);
+	#endif
 	if(HAL_I2C_IsDeviceReady(&_EEPROM24XX_I2C,0xa0,1,100)==HAL_OK)
 		return true;
 	else
@@ -28,6 +31,10 @@ bool		EEPROM24XX_Save(uint16_t Address,void *data,size_t size_of_data)
 		return false;
 	#endif
 	
+	#if	(_EEPROM_USE_WP_PIN==1)
+	HAL_GPIO_WritePin(_EEPROM_WP_GPIO,_EEPROM_WP_PIN,GPIO_PIN_RESET);
+	#endif
+	
 	#if ((_EEPROM_SIZE_KBIT==1) || (_EEPROM_SIZE_KBIT==2))
 	if(HAL_I2C_Mem_Write(&_EEPROM24XX_I2C,0xa0,Address,I2C_MEMADD_SIZE_8BIT,(uint8_t*)data,size_of_data,100) == HAL_OK)
 	#else
@@ -39,14 +46,25 @@ bool		EEPROM24XX_Save(uint16_t Address,void *data,size_t size_of_data)
 		#else
 		HAL_Delay(7);
 		#endif
+		#if	(_EEPROM_USE_WP_PIN==1)
+		HAL_GPIO_WritePin(_EEPROM_WP_GPIO,_EEPROM_WP_PIN,GPIO_PIN_SET);
+		#endif
 		return true;
 	}
 	else
+	{
+		#if	(_EEPROM_USE_WP_PIN==1)
+		HAL_GPIO_WritePin(_EEPROM_WP_GPIO,_EEPROM_WP_PIN,GPIO_PIN_SET);
+		#endif
 		return false;		
+	}
 }
 //##########################################################################
 bool		EEPROM24XX_Load(uint16_t Address,void *data,size_t size_of_data)
 {
+	#if	(_EEPROM_USE_WP_PIN==1)
+	HAL_GPIO_WritePin(_EEPROM_WP_GPIO,_EEPROM_WP_PIN,GPIO_PIN_SET);
+	#endif
 	#if ((_EEPROM_SIZE_KBIT==1) || (_EEPROM_SIZE_KBIT==2))
 	if(HAL_I2C_Mem_Read(&_EEPROM24XX_I2C,0xa0,Address,I2C_MEMADD_SIZE_8BIT,(uint8_t*)data,size_of_data,100) == HAL_OK)
 	#else
--- eeprom24xxConfig.h
+++ eeprom24xxConfig.h
@@ -2,9 +2,15 @@
 #define	_24XXCONFIG_H
 
 
-#define		_EEPROM_SIZE_KBIT										64
+#define		_EEPROM_SIZE_KBIT										32
 #define		_EEPROM24XX_I2C											hi2c1			
 #define		_EEPROM_FREERTOS_IS_ENABLE					1
+#define		_EEPROM_USE_WP_PIN									1
+
+#if (_EEPROM_USE_WP_PIN==1)
+#define		_EEPROM_WP_GPIO											EE_WP_GPIO_Port
+#define		_EEPROM_WP_PIN											EE_WP_Pin
+#endif
 
 #endif
 
