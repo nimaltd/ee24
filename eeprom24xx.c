4b6e8896078a31959ee3d54b7048e8a682431e92
Nima Askari ----- نیما عسکری <nimaltd@yahoo.com>
2020-03-09 12:27:18 +0330

    Update eeprom24xx.c

--- eeprom24xx.c
+++ eeprom24xx.c
@@ -76,7 +76,7 @@ bool		EEPROM24XX_Load(uint16_t Address,void *data,size_t size_of_data)
 	#elif (_EEPROM_SIZE_KBIT==8)
 	if(HAL_I2C_Mem_Read(&_EEPROM24XX_I2C,0xa0|((Address&0x0300>>7)),(Address&0xff),I2C_MEMADD_SIZE_8BIT,(uint8_t*)data,size_of_data,100) == HAL_OK)
 	#elif (_EEPROM_SIZE_KBIT==16)
-	if(HAL_I2C_Mem_Write(&_EEPROM24XX_I2C,0xa0|((Address&0x0700>>7)),(Address&0xff),I2C_MEMADD_SIZE_8BIT,(uint8_t*)data,size_of_data,100) == HAL_OK)
+	if(HAL_I2C_Mem_Read(&_EEPROM24XX_I2C,0xa0|((Address&0x0700>>7)),(Address&0xff),I2C_MEMADD_SIZE_8BIT,(uint8_t*)data,size_of_data,100) == HAL_OK)
 	#else
 	if(HAL_I2C_Mem_Read(&_EEPROM24XX_I2C,0xa0,Address,I2C_MEMADD_SIZE_16BIT,(uint8_t*)data,size_of_data,100) == HAL_OK)
 	#endif
